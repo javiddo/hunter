1191c18ebd2b3bc802c59cbf26c9f78720708cf1
Javid Aliyev <javid.aliyev@mps.az>
2023-02-09 10:05:48 +0000

    .

---
+++ SQL/posts_export_2023-02-09_140423.sql
@@ -0,0 +1,2 @@
+insert into `posts` (`content`, `created_at`, `description`, `id`, `owner_id`, `title`) values ('test content', '2023-02-09 12:43:07', 'test description 888', 1, 1, 'test title 1111');
+insert into `posts` (`content`, `created_at`, `description`, `id`, `owner_id`, `title`) values ('second content', '2023-02-09 12:44:13', 'second description', 2, 1, 'second title');
---
+++ SQL/users_export_2023-02-09_140437.sql
@@ -0,0 +1,2 @@
+insert into `users` (`created_at`, `email`, `id`, `name`, `password`) values ('2023-02-09 12:24:42', 'admin@admin.com', 1, 'Javid', '$2y$10$M3zy50btjwS/HroZxoYwGOqYL65LMano7MKhFMJcrLL192eydqFIm');
+insert into `users` (`created_at`, `email`, `id`, `name`, `password`) values ('2023-02-09 14:00:46', 'test@test.com', 2, 'Test', '$2y$10$cBVCRiD5IhKtm8WjrcPu7uXpZH8HMrLUj4jL.PCRcZUcN1RNe0Fs.');
